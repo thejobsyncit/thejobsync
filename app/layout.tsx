9c63daa8fb5c9b7e3344f5c0facfa13fa36164d5
M R KAMESH <115403726+KAMESH1206@users.noreply.github.com>
2026-08-05 16:11:04 +0530

    chore: Add Google Analytics and Google Tag Manager scripts

--- app/layout.tsx
+++ app/layout.tsx
@@ -189,6 +189,28 @@ export default function RootLayout({
             }
           `}
         </Script>
+        
+        {/* Google Analytics */}
+        <Script src="https://www.googletagmanager.com/gtag/js?id=G-V3V71XSR53" strategy="afterInteractive" />
+        <Script id="google-analytics" strategy="afterInteractive">
+          {`
+            window.dataLayer = window.dataLayer || [];
+            function gtag(){dataLayer.push(arguments);}
+            gtag('js', new Date());
+            gtag('config', 'G-V3V71XSR53');
+          `}
+        </Script>
+        
+        {/* Google Tag Manager */}
+        <Script id="google-tag-manager" strategy="afterInteractive">
+          {`
+            (function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+            new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+            j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+            'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+            })(window,document,'script','dataLayer','GTM-WD3S5W42');
+          `}
+        </Script>
         <style dangerouslySetInnerHTML={{ __html: `
           body { top: 0 !important; }
           .skiptranslate iframe, .goog-te-banner-frame { display: none !important; }
